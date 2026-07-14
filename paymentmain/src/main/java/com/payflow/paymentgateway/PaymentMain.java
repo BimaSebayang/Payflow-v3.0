075523647dad963236e72d6e50ea5afbd3148bf3
Bima Satrya Sebayang <34059058+BimaSebayang@users.noreply.github.com>
2026-07-14 09:36:12 +0700

    feat : Template Project
    feat : Template Project
    
    feat : Template Project

---
+++ paymentmain/src/main/java/com/payflow/paymentgateway/PaymentMain.java
@@ -0,0 +1,8 @@
+package com.payflow.paymentgateway;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+@SpringBootApplication
+public class PaymentMain {
+ public static void main(String[] args){
+   SpringApplication.run(com.payflow.paymentgateway.PaymentMain.class,args);
+ }}
---
+++ paymentscheduler/src/main/java/com/payflow/paymentscheduler/PaymentScheduler.java
@@ -0,0 +1,8 @@
+package com.payflow.paymentgateway;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+@SpringBootApplication
+public class PaymentScheduler {
+ public static void main(String[] args){
+   SpringApplication.run(com.payflow.paymentgateway.PaymentScheduler.class,args);
+ }}
